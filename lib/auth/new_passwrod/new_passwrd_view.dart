12f6bc0a5680cf7a9396019860402772d081f328
nader <nadersameer@std.mans.edu.eg>
2026-04-19 03:50:16 +0200

    Finish of NewPasswrod view

--- lib/auth/new_passwrod/new_passwrd_view.dart
+++ lib/auth/new_passwrod/new_passwrd_view.dart
@@ -1,10 +1,72 @@
+import 'package:etmaen/core/logic/app_routes.dart';
+import 'package:etmaen/core/ui/app_back.dart';
+import 'package:etmaen/core/ui/app_button.dart';
+import 'package:etmaen/core/ui/app_input.dart';
+import 'package:etmaen/core/ui/app_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NewPasswordView extends StatelessWidget {
   const NewPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 20.h,
+              ),
+              const AppBack(),
+              SizedBox(
+                height: 40.h,
+              ),
+              Text(
+                'ادخل كلمة مرور جديدة',
+                style: AppStyle.regular28.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Text(
+                'ادخل كلمة المرور ',
+                style: AppStyle.regular16,
+              ),
+
+              AppInput(
+                topSpacing: 4.h,
+                bottomSpacing: 8.h,
+                hintText: 'مثل : Etmaen@77',
+                prefixIcon: 'lock.svg',
+              ),
+              Text(
+                'تأكيد كلمة المرور ',
+                style: AppStyle.regular16,
+              ),
+              AppInput(
+                topSpacing: 4.h,
+                bottomSpacing: 8.h,
+                hintText: 'مثل : Etmaen@77',
+                prefixIcon: 'lock.svg',
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              AppButton(
+                onTap: () => Navigator.pushNamed(context, AppRoutes.login),
+                title: 'كلمه مرور جديده',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
